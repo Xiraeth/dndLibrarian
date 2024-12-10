285cf3cd8de8e2ab9965aa597ec73c9a5344ee32
nick <nickiliadis99@gmail.com>
2024-12-10 19:06:59 +0200

    change login logic

--- routes/login.js
+++ routes/login.js
@@ -8,12 +8,27 @@ router.get("/", function (req, res, next) {
   res.render("login", { title: "Log in", error: errorMessage });
 });
 
-router.post(
-  "/",
-  passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/login?error=invalid",
-  })
-);
+router.post("/", (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) {
+      console.error("Login error:", err);
+      return next(err);
+    }
+
+    if (!user) {
+      console.log("Authentication failed:", info.message);
+      return res.redirect("/login?error=invalid");
+    }
+
+    req.logIn(user, (err) => {
+      if (err) {
+        console.error("Session error:", err);
+        return next(err);
+      }
+      console.log("User logged in successfully:", user.username);
+      return res.redirect("/");
+    });
+  })(req, res, next);
+});
 
 module.exports = router;
